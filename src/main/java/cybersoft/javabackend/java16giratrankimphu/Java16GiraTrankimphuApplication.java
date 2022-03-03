76cb8bfe04537cedd650b5637f9ead6d1c7f59c8
tkphu <95673369+tkphu@users.noreply.github.com>
2022-03-03 20:59:01 +0700

    java16 - gira - trankimphu

---
+++ src/main/java/cybersoft/javabackend/java16giratrankimphu/Java16GiraTrankimphuApplication.java
@@ -0,0 +1,13 @@
+package cybersoft.javabackend.java16giratrankimphu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java16GiraTrankimphuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Java16GiraTrankimphuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cybersoft/javabackend/java16giratrankimphu/Java16GiraTrankimphuApplicationTests.java
@@ -0,0 +1,13 @@
+package cybersoft.javabackend.java16giratrankimphu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java16GiraTrankimphuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
